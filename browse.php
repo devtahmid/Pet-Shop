b89af8158263b3b159f8b7ca12796ffb0f183b43
devtahmiddfdfdf <shaikhtahmidurrahman1@gmail.com>
2023-01-01 14:40:19 +0300

    view more page done, need to fix table values

--- browse.php
+++ browse.php
@@ -4,9 +4,7 @@ require("project_connection.php");
 session_start();
 if (!isset($_SESSION['userId']))
   header('location:../login_form.php');
-/* if ($_SESSION['userType'] == 'admin')) {
 
-} */
 try {
   $sql = "SELECT * FROM SERVICES";
   $result = $db->query($sql);
@@ -16,14 +14,66 @@ try {
   die("Error Message" . $e->getMessage());
 }
 
-if ($result->rowcount() > 0) {
+?>
 
-  foreach ($result as $row) {
 
-    echo $row['NAME'] . "<br/>" . "BD " . $row['PRICE'] . "<br /> <img src='./image/" . $row['PICTURE'] . "' width='100' height='100'>";
-    echo "<form method='get' action='viewmore.php'>";
-    echo "<input type='hidden' name='id' value='" . $row["ID"] . "'/>";
-    echo "<input type='submit' name='view' value='View More Details'/>";
-    echo "</form>";
-  }
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Browse</title>
+  <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <?php
+  if ($_SESSION['userType'] == 'admin') {
+    require('adminnavbar.php');
+  } /* else if(){
 }
+ */
+  ?>
+  <br /><br /><br />
+
+  <main class="container-xl text-center">
+    <div class="row">
+      <?php
+
+      if ($result->rowcount() > 0) {
+
+        foreach ($result as $row) {
+
+      ?>
+          <div class="col mt-3  mx-auto">
+            <div class="card" style="width: 18rem;">
+              <img src="./image/<?php echo $row['PICTURE']; ?>">
+              <div class="card-body">
+
+
+                <h3 class="card-title"> <?php echo $row['NAME']; ?></h3>
+                <p class="card-text">BHD <?php echo $row['PRICE']; ?></p>
+                <form method='get' action='viewmore.php'>
+                  <input type='hidden' name='id' value='<?php echo $row["ID"]; ?>' />
+                  <input type='submit' class="btn btn-primary" name='view' value='View More Details' />
+                </form>
+              </div>
+            </div>
+          </div>
+      <?php
+
+        }
+      }
+      ?>
+    </div>
+  </main>
+
+
+
+
+</body>
+
+</html>
--- viewmore.php
+++ viewmore.php
@@ -1,5 +1,6 @@
 <?php
-require("connection.php");
+require("project_connection.php");
+session_start();
 extract($_GET); //$id
 try {
   $sql1 = "SELECT * FROM services WHERE ID= :id";
@@ -16,25 +17,93 @@ try {
 } catch (PDOException $e) {
   die("Error Message" . $e->getMessage());
 }
-
-if ($result->rowcount() > 0) {
-
-  foreach ($result as $row) {
-
-    echo $row['NAME'] . "<br/>" . "BD " . $row['PRICE'] . "<br/>" . "Rating: " . $row['RATING'] . "/5" . "<br /> <img src='./image/" . $row['PICTURE'] . "' width='100' height='100'>";
-
-    echo "Timings:- <br />";
-    echo "Time slot ---- Sunday ----Monday --- Tuesday ---- Wednesday ---- Thursday  <br/>";
-    foreach ($result2 as $row2) {
-      echo $row2['TIME_SLOT_START'] . "-" . $row2['TIME_SLOT_END'] . "----";
-      echo $row2['SUNDAY'] . "----" . $row2['MONDAY'] . "----" . $row2['TUESDAY'] . "----" . $row2['WEDNESDAY'] . "----" . $row2['THURSDAY'];
-      echo "<br/>";
-    }
+?>
 
 
-    echo "<form method='get' action='viewavailabilities.php'>";
-    echo "<input type='hidden' name='id' value='" . $row["ID"] . "'/>";
-    echo "<input type='submit' name='book' value='Book Service'/>";
-    echo "</form>";
-  }
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>View Item</title>
+  <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php
+  if ($_SESSION['userType'] == 'admin') {
+    require('adminnavbar.php');
+  } /* else if(){
 }
+ */
+  ?>
+
+  <br /><br /><br /><br />
+
+  <main class="container-xl">
+    <?php
+
+    if ($result->rowcount() > 0) {
+
+      foreach ($result as $row) {
+    ?>
+        <div class="row">
+          <div class="col">
+            <img class="img-fluid" src='./image/<?php echo $row['PICTURE'] ?>'>
+          </div>
+          <div class="col">
+            <div class="row text-center">
+              <div class="col-xs-12 mb-3">
+                <h3> <?php echo $row['NAME']; ?> </h3>
+              </div>
+              <div class="col-xs-12 mb-3">
+                <b>Price:</b> BHD <?php echo  $row['PRICE']; ?>
+              </div>
+              <div class="col-xs-12">
+                <b>Rating:</b> <?php echo $row['RATING']; ?> / 5
+              </div>
+            </div>
+          </div>
+        </div>
+        <h4 class="mt-3 mb-2"> Working hours (Check availabilitites to book a time):-</h4>
+        <div class="table-responsive">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Time Slot</th>
+                <th scope="col">Sunday</th>
+                <th scope="col">Monday</th>
+                <th scope="col">Tuesday</th>
+                <th scope="col">Wednesday</th>
+                <th scope="col">Thursday</th>
+              </tr>
+            </thead>
+            <tbody class="table-group-divider">
+              <?php
+              foreach ($result2 as $row2) {
+
+                echo "<tr>";
+                echo "<th scope='row'>" . $row2['TIME_SLOT_START'] . "-" . $row2['TIME_SLOT_END'] . "</th>";
+                echo "<td>" . $row2['SUNDAY'] . "</td>";
+                echo "<td>" . $row2['MONDAY'] . "</td>";
+                echo "<td>" . $row2['TUESDAY'] . "</td>";
+                echo "<td>" . $row2['WEDNESDAY'] . "</td>";
+                echo "<td>" . $row2['THURSDAY'] . "</td>";
+                echo "</tr>";
+              }
+              ?>
+            </tbody>
+          </table>
+        </div>
+        <div class="container-xl text-center">
+          <form method='get' action='viewavailabilities.php'>
+            <input type='hidden' name='id' value='<?php echo $row["ID"]; ?>' />
+            <input type='submit' name='book' class="btn btn-primary" value='Check Availabilities' />
+          </form>
+        </div>
+    <?php
+      }
+    }
+    ?>
